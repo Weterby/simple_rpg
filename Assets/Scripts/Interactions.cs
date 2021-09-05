34231f159f7ce4284ca0db38f3c6928cae14ec38
Adam <weterby2@gmail.com>
2021-09-05 17:59:16 +0200

    Create and configure NavMesh system

---
+++ Assets/Scripts/Interactions.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Interactions : MonoBehaviour
+{
+    NavMeshAgent playerAgent;
+
+    private void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            GetInteraction();
+        }
+    }
+
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if(interactedObject.tag == "Interactable")
+            {
+                Debug.Log("Interacted with " + interactedObject.name);
+            }
+            else
+            {
+                playerAgent.SetDestination(interactionInfo.point);
+            }
+        }
+    }
+}
